3f83b80b4847a4189a5633d0d43cc6080fe39537
MalinoSky <dtimoshenko2080@mail.ru>
2016-10-13 13:16:58 +0300

    Add kb
    Add kb

---
+++ kb/js/common.js
@@ -0,0 +1,10 @@
+$(window).load(function(){
+	$("[data-toggle]").click(function() {
+	var toggle_el = $(this).data("toggle");
+	$(toggle_el).toggleClass("open-sidebar");
+	});
+});
+
+$('.arrow_clicked').click(function () {
+	$(this).find('span').toggleClass('actived');
+});
